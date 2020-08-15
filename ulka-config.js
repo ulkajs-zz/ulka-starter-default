7d3902bd9dc781cf52cbf1b17d26c61f7ad24dad
acharyaroshanji <acharyaroshan2357@gmail.com>
2020-08-15 13:05:16 +0545

    ulka-plugin-sitemap added

--- ulka-config.js
+++ ulka-config.js
@@ -1,39 +1,34 @@
 // All the configurations are changable
 
 module.exports = {
-  // siteMetaData is available inside all md and ulka files
   siteMetaData: {
     title: "UlkaJs",
     description: "This is a default starter for ulka js",
+    domain: "https://ulka-starter-default.com.np",
   },
-  // Build path from root dir
   buildPath: "build",
-  // Path to the directory of ulka files from src dir
   pagesPath: "pages",
-  //   Path to the templates dir from src dir
   templatesPath: "templates",
   plugins: [
     "ulka-plugin-reading-time",
     {
       resolve: "ulka-remarkable-link-headers",
       options: {
-        hLevels: [1, 2],
+        hLevels: [1, 2, 3],
         setAnchor: false,
         anchorValue: "# ",
       },
     },
+    {
+      resolve: "ulka-plugin-sitemap",
+      options: {
+        lastmod: true,
+      },
+    },
   ],
   contents: {
-    // Path to the directory of markdown files from src dir
     path: "contents",
-    /**
-     * Directory to include all contents
-     * for eg:
-     * post-1.md => blog/post-1.html
-     * tutorials/part-1/index.md => blog/tutorials/part-1/index.md
-     */
     generatePath: "blog",
-    // File inside `templatesPath` which is used by contents
     template: "blog.ulka",
   },
 };
