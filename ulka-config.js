5e69660f760c1a4bf001883b1b9e1b2c7d59211c
Roshan Acharya <acharyaroshan2357@gmail.com>
2020-10-13 16:55:35 +0545

    fix: removed plugins

--- ulka-config.js
+++ ulka-config.js
@@ -2,37 +2,28 @@
 
 module.exports = {
   siteMetaData: {
-    title: "UlkaJs",
-    description: "This is a default starter for ulka js",
-    domain: "https://ulka-starter-default.netlify.app/",
+    title: 'UlkaJs',
+    description: 'This is a default starter for ulka js',
+    domain: 'https://ulka-starter-default.netlify.app/',
   },
-  buildPath: "build",
-  pagesPath: "pages",
-  templatesPath: "templates",
+  buildPath: 'build',
+  pagesPath: 'pages',
+  templatesPath: 'templates',
   plugins: [
-    "ulka-plugin-reading-time",
+    'ulka-plugin-reading-time',
     {
-      resolve: "ulka-plugin-autolink-headers",
+      resolve: 'ulka-plugin-autolink-headers',
       options: {
-        elements: ["h1", "h2", "h3", "h4"],
+        elements: ['h1', 'h2', 'h3', 'h4'],
       },
     },
-    "ulka-plugin-sitemap",
-    {
-      resolve: "ulka-plugin-remark-emoji",
-      options: {
-        emoticon: true,
-        padSpaceAfter: true,
-      },
-    },
-    "ulka-plugin-accessible-emoji",
-    "ulka-plugin-rehype-highlight",
+    'ulka-plugin-sitemap',
   ],
   contents: [
     {
-      path: "contents",
-      generatePath: "blog",
-      template: "blog.ulka",
+      path: 'contents',
+      generatePath: 'blog',
+      template: 'blog.ulka',
     },
   ],
-};
+}
