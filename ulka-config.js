202e723ee7d74bbd175063a2c5e82d5d043062e4
Roshan Acharya <acharyaroshan2357@gmail.com>
2020-10-21 16:42:13 +0545

    add 404 ejs page

--- ulka-config.js
+++ ulka-config.js
@@ -2,12 +2,13 @@ module.exports = {
   siteMetaData: {
     title: 'UlkaJs',
     description: 'This is a default starter for ulka js',
-    domain: 'https://ulka-starter-default.netlify.app/',
+    domain: 'https://ulka-starter-default.netlify.app',
   },
   buildPath: 'build',
   pagesPath: 'pages',
   templatesPath: 'templates',
   plugins: [
+    'ulka-source-ejs',
     'ulka-plugin-sitemap',
     'ulka-plugin-reading-time',
     'ulka-plugin-autolink-headers',
